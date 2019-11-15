cdaa32746ae68a74eef380f00f2090c61393d151
yuhaihe <yhh@kyong.net>
2019-11-15 10:51:39 +0800

    登录注册模块

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -9,7 +9,7 @@ const server = require('../server')
 const userName = `u_${Date.now()}`
 const password = `p_${Date.now()}`
 const testUser = {
-  userName, 
+  userName,
   password,
   nickName: userName,
   gender: 1
@@ -32,7 +32,7 @@ test('用户注册，应该失败', async () => {
 
 // 查询用户是否存在
 test('查询用户注册名，应该存在', async () => {
-  const res = await server.post('/api/user/isExist').send({userName})
+  const res = await server.post('/api/user/isExist').send({ userName })
   expect(res.body.errno).toBe(0)
 })
 
@@ -42,13 +42,28 @@ test('非法注册，应该失败', async () => {
     .send({
       userName: '123',
       password: '123',
-      gender: 2
+      gender: 'mail'
     })
   expect(res.body.errno).not.toBe(0)
 })
 
 // 登录验证
 test('登录验证，应该成功', async () => {
-  const res = await server.post('/api/user/login').send({userName,password})
+  const res = await server.post('/api/user/login').send({ userName, password })
+  expect(res.body.errno).toBe(0)
+
+  // 获取cookie
+  COOKIE = res.headers['set-cookie'].join(';')
+})
+
+// 删除用户
+test('删除，应该成功', async () => {
+  const res = await server.post('/api/user/delete').set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+// 再次查询用户，应该不存在
+test('再次用户注册名，应该不存在', async () => {
+  const res = await server.post('/api/user/isExist').send({ userName })
   expect(res.body.errno).not.toBe(0)
 })
